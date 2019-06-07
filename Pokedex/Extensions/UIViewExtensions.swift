2fd96c98ee9a7ca32248386d770385a0f2e4a860
Juliano <jnardon@gmail.com>
2019-06-07 14:52:01 -0300

    initial commit

---
+++ Pokedex/Extensions/UIViewExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewExtensions.swift
+//  Pokedex
+//
+//  Created by Juliano Nardon on 07/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get { return layer.borderColor.map(UIColor.init) }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get { return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+    }
+}
---
+++ Pokedex/Models/PokemonType.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  Pokedex
+//
+//  Created by Juliano Nardon on 07/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+enum PokemonType: String {
+    case bug
+    case dark
+    case dragon
+    case eletric
+    case fairy
+    case fight
+    case fire
+    case flying
+    case ghost
+    case grass
+    case ground
+    case ice
+    case normal
+    case poison
+    case psychic
+    case rock
+    case steel
+    case water
+    
+    var icon: UIImage? {
+        return UIImage(named: "ic_\(self.rawValue)")
+    }
+    
+    var color: UIColor? {
+        return UIColor(named: "\(self.rawValue)")
+    }
+}
