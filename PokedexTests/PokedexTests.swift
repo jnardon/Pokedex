48294aafccfc2a2229c45575af4f69b824f648a5
Marcelo Gobetti <mwgobetti@gmail.com>
2019-06-09 14:49:39 -0700

    naive async test "passing"

--- PokedexTests/PokedexTests.swift
+++ PokedexTests/PokedexTests.swift
@@ -19,10 +19,17 @@ class PokedexTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testDecoding() {
         let jsonURL = Bundle(for: PokedexTests.self).url(forResource: "pokemons", withExtension: "json")!
         let data = try! Data(contentsOf: jsonURL)
 
         XCTAssertNoThrow(try Pokedex.RequestMaker.decoder.decode(PokemonResponse.self, from: data))
+    }
+
+    func testRequestList() {
+        let requestMaker = RequestMaker()
+        requestMaker.make(withEndpoint: .list) { (list: PokemonList) in
+            XCTAssertGreaterThan(list.pokemons.count, 0)
+        }
     }
 }
