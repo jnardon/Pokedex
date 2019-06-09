1b5dc5a9d61fb64059cfd060b9d8c209c94f41c2
Marcelo Gobetti <mwgobetti@gmail.com>
2019-06-09 15:22:51 -0700

    move Presenter to its own file

---
+++ Pokedex/PokemonList/PokemonListPresenter.swift
@@ -0,0 +1,55 @@
+//
+//  PokemonListPresenter.swift
+//  Pokedex
+//
+//  Created by Marcelo Gobetti on 6/9/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+class PokemonListPresenter: NSObject {
+
+    weak var view: PokemonListViewType?
+
+    private let requestMaker = RequestMaker()
+
+    private var pokemonList = [Pokemon]()
+
+}
+
+extension PokemonListPresenter: PokemonListPresenterType {
+
+    func fetchData() {
+        requestMaker.make(withEndpoint: .list) { (pokemonList: PokemonList) in
+            self.pokemonList = pokemonList.pokemons
+
+            DispatchQueue.main.async {
+                self.view?.reloadData()
+            }
+        }
+    }
+
+    func pokemon(at index: Int) -> Pokemon {
+        return self.pokemonList[index]
+    }
+
+}
+
+extension PokemonListPresenter: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.pokemonList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemon", for: indexPath)
+
+        if let pokemonCell = cell as? PokemonTableViewCell {
+            pokemonCell.config(with: self.pokemonList[indexPath.row])
+        }
+
+        return cell
+    }
+
+}
--- Pokedex/PokemonList/PokemonListViewController.swift
+++ Pokedex/PokemonList/PokemonListViewController.swift
@@ -37,33 +37,6 @@ extension PokemonListViewController: PokemonListViewType {
     }
 }
 
-class PokemonListPresenter: NSObject {
-
-    weak var view: PokemonListViewType?
-
-    private let requestMaker = RequestMaker()
-
-    private var pokemonList = [Pokemon]()
-
-}
-
-extension PokemonListPresenter: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.pokemonList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemon", for: indexPath)
-        
-        if let pokemonCell = cell as? PokemonTableViewCell {
-            pokemonCell.config(with: self.pokemonList[indexPath.row])
-        }
-        
-        return cell
-    }
-
-}
-
 extension PokemonListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -73,22 +46,4 @@ extension PokemonListViewController: UITableViewDelegate {
             self.navigationController?.present(detailViewController, animated: true)
         }
     }
-}
-
-extension PokemonListPresenter: PokemonListPresenterType {
-
-    func fetchData() {
-        requestMaker.make(withEndpoint: .list) { (pokemonList: PokemonList) in
-            self.pokemonList = pokemonList.pokemons
-            
-            DispatchQueue.main.async {
-                self.view?.reloadData()
-            }
-        }
-    }
-
-    func pokemon(at index: Int) -> Pokemon {
-        return self.pokemonList[index]
-    }
-
 }
